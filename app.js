8945724c3b5dd9e60bc69c59e6c35addb3e09d79
victorien <deadvicmangame@gmail.com>
2022-12-06 11:00:33 +0100

    exo 86

--- app.js
+++ app.js
@@ -31,10 +31,27 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [
+        peugeot,
+        citroen,
+        ford,
+    ], // Vos voitures dans cette propriété !
+}
+for (let i = 0; i < voitures.collection.length; i++) {
+    voitures.collection[i].ouvrirPorte();
 }
 
-// FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
+para = document.createElement("p");
+document.body.append(para);
+
+for (let i = 0; i < voitures.collection.length; i++) {
+    let carColor = document.createElement("span");
+    carColor.innerText = voitures.collection[i].color; //<span>blue</span>
+    console.log(carColor);
+    para.innerHTML += carColor.innerHTML;
+}
+
+// FIXME Parcourez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
 
 
